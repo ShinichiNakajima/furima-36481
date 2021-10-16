5672e1264244f9a9e1eedadb206d144e535b8528
ShinichiNakajima <ShinichiTheNakaG@gmail.com>
2021-10-16 11:51:15 +0900

    Userモデル単体テスト作成

---
+++ spec/factories/users.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :user do
+    nickname              {'TEST'}
+    email                 {'test@example'}
+    password              {'000aaa'}
+    password_confirmation {password}
+    last_name             {"名字田"}
+    first_name            {"名前郎"}
+    last_name_kana        {"ミョウジ"}
+    first_name_kana       {"ナマエ"}
+    birth_date            {Date.new(1999, 12, 31)}
+  end
+end
