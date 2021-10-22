5a5e59533da8f45e242af61416fb39425d9d4c28
ShinichiNakajima <ShinichiTheNakaG@gmail.com>
2021-10-22 20:30:59 +0900

    商品登録ページ編集&マイグレーションミス修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,13 +1,16 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :user
-  has_one    :purchase
   belongs_to :category
   belongs_to :sales_status
   belongs_to :shipping_fee_status
   belongs_to :prefecture
   belongs_to :scheduled_delivery
+
+  belongs_to       :user
+  has_one          :purchase
+  has_one_attached :image
   
-  validates :purchase_id, :category_id, :sales_status_id, :shipping_fee_status_id, :prefecture_id, :scheduled_delivery_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :name, :info, :price, presence: true
+  validates :category_id, :sales_status_id, :shipping_fee_status_id, :prefecture_id, :scheduled_delivery_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :name, :info, :price, :image, presence: true
+  validates :price, inclusion: { in: 300..9999999 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -40,7 +40,7 @@ ActiveRecord::Schema.define(version: 2021_10_16_125857) do
     t.integer "sales_status_id", null: false
     t.integer "shipping_fee_status_id", null: false
     t.integer "prefecture_id", null: false
-    t.integer "schedule_delivery_id", null: false
+    t.integer "scheduled_delivery_id", null: false
     t.integer "price", null: false
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
