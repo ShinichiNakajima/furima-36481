4411fa121699974eeab605cf19d08b11db4d2812
ShinichiNakajima <ShinichiTheNakaG@gmail.com>
2021-10-25 21:41:21 +0900

    テストコード実装＆エラーメッセージ整頓

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,8 +9,15 @@ class Item < ApplicationRecord
   belongs_to       :user
   has_one          :purchase
   has_one_attached :image
-  
+
+  validates :image, :name, :info, presence: true
+
+  validates :price, presence: true
+  validates :price, format: {with: /\A[0-9]+\z/, message: 'is invalid. Input half-width characters'}
+  validates :price, inclusion: { in: 300..9999999, message: "is out of setting range" }
+
   validates :category_id, :sales_status_id, :shipping_fee_status_id, :prefecture_id, :scheduled_delivery_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :name, :info, :price, :image, presence: true
-  validates :price, inclusion: { in: 300..9999999 }
+  validates :category_id, :sales_status_id, :shipping_fee_status_id, :prefecture_id, :scheduled_delivery_id, presence: true
+
+
 end
