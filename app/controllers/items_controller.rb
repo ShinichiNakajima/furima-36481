dc1934b61043d9e339bd6ebeb9d486448ad8df1e
ShinichiNakajima <ShinichiTheNakaG@gmail.com>
2021-11-06 12:53:41 +0900

    Rubocop実行

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -36,9 +36,7 @@ class ItemsController < ApplicationController
 
   def destroy
     item = Item.find(params[:id])
-    if user_signed_in? && item.user.id == current_user.id
-      item.destroy
-    end
+    item.destroy if user_signed_in? && item.user.id == current_user.id
     redirect_to root_path
   end
 
