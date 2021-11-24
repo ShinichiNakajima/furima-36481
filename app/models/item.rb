e997c93580d16a0da28bf096146d350e926315a0
ShinichiNakajima <ShinichiTheNakaG@gmail.com>
2021-11-24 21:16:23 +0900

    モデル書き換え

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,7 +7,7 @@ class Item < ApplicationRecord
   belongs_to :scheduled_delivery
 
   belongs_to       :user
-  has_one          :purchase
+  has_one          :order
   has_one_attached :image
 
   validates :image, :name, :info, presence: true
